867fe380ef21e1e30c50b35f6b0a48a1dbc9c9df
“[Nathaniel <[omokhodionn@gmail.com]>
2025-09-04 17:41:39 +0100

    Fix: cleaned dependencies and updated ServiceCard

--- src/app/components/Home/Navbar/MobileNav.tsx
+++ src/app/components/Home/Navbar/MobileNav.tsx
@@ -3,38 +3,46 @@ import Link from "next/link";
 import React from "react";
 import { CgClose } from "react-icons/cg";
 
-// Define props type
 type Props = {
   showNav: boolean;
   closeNav: () => void;
 };
 
 const MobileNav = ({ closeNav, showNav }: Props) => {
-  const navopen = showNav ? "translate-x-0" : "translate-x-[-100%]";
+  // Tailwind has -translate-x-full, no need for translate-x-[-100%]
+  const navopen = showNav ? "translate-x-0" : "-translate-x-full";
+
   return (
-    <div>
-      {/* Mobile navigation content can be added here */}
+    // 👇 ensures it only shows on mobile/tablet
+    <div className="block lg:hidden">
+      {/* Overlay */}
       <div
-        className={`fixed ${navopen} transform transition-all duration-500 inset-0 z-[1000] bg-white-800 opacity-70 w-full h-screen`}
+        className={`fixed inset-0 z-[1000] bg-black/70 transition-opacity duration-500 ${
+          showNav ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+        onClick={closeNav}
       ></div>
-      {/* Nav links items can be added here */}
+
+      {/* Sidebar */}
       <div
-        className={`text-black ${navopen} transform transition-all duration-500 delay-300 fixed justify-center  flex flex-col h-full w-[80%] sm:w-[60] bg-[#502556] space-x-6 z-[10000]`}
+        className={`text-white ${navopen} transform transition-transform duration-500 fixed top-0 left-0 flex flex-col h-full w-[80%] sm:w-[60%] bg-[#502556] z-[10000]`}
       >
-        {navLinks.map((navlink) => {
-          return (
-            <Link key={navlink.id} href={navlink.url}>
-              <p className="nav__link text-[20px] ml-12 border-b-[1.5px] pb-2 border-b-white sm:text-[40px] bottom-[10rem]">
+        {/* Close button */}
+        <CgClose
+          onClick={closeNav}
+          className="absolute top-4 right-4 sm:w-8 sm:h-8 w-6 h-6 text-white cursor-pointer"
+        />
+
+        {/* Links */}
+        <div className="mt-16 space-y-6 pl-12">
+          {navLinks.map((navlink) => (
+            <Link key={navlink.id} href={navlink.url} onClick={closeNav}>
+              <p className="text-lg border-b border-white pb-2 sm:text-2xl">
                 {navlink.label}
               </p>
             </Link>
-          );
-        })}
-        {/* closed button */}
-        <CgClose
-          onClick={closeNav}
-          className="absolute top-[0.7rem] right-[0.12rem] sm:w-8 sm:h-8 w-6 h-6 text-black  "
-        />
+          ))}
+        </div>
       </div>
     </div>
   );
